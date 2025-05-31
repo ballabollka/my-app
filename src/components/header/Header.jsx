68a5e0704795f3b06bf2e5ad6622769bc2cbd185
ballabollka <goncarovadara454@gmail.com>
2025-05-31 20:09:47 +1000

    Обновление проекта

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import comp from './../../img/comp.png'
 import './Header.css'
 import { Link, useLocation } from 'react-router-dom'
 
 const Header = () => {
   const location = useLocation()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   const isKursPage = location.pathname === '/kurs'
   const isContactsPage = location.pathname === '/contacts'
   const isFreeLessonPage = location.pathname === '/freelesson'
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
 
   return (
     <main>
@@ -15,38 +21,46 @@ const Header = () => {
         <div className='container'>
           <div className='content'>
             <div className='header_logo'>
-              <img src={comp} alt='ProgramSchool'></img>
+              <img src={comp} alt='ProgramSchool' />
               <span>ProgramSchool</span>
             </div>
-            <div className='header_nav'>
+
+            {/* Бургер меню для мобильных */}
+            <div className='burger' onClick={toggleMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+
+            {/* Навигация */}
+            <div className={`header_nav ${isMenuOpen ? 'active' : ''}`}>
               <ul>
-                {/* Первая ссылка - на главную или курсы */}
                 {isKursPage || isContactsPage || isFreeLessonPage ? (
-                  <li><Link to="/">ГЛАВНАЯ</Link></li>
+                  <li><Link to="/" onClick={() => setIsMenuOpen(false)}>ГЛАВНАЯ</Link></li>
                 ) : (
-                  <li><Link to="/kurs">КУРСЫ</Link></li>
+                  <li><Link to="/kurs" onClick={() => setIsMenuOpen(false)}>КУРСЫ</Link></li>
                 )}
 
-                {/* Вторая ссылка - курсы или о нас */}
                 {isFreeLessonPage ? (
-                  <li><Link to="/kurs">КУРСЫ</Link></li>
+                  <li><Link to="/kurs" onClick={() => setIsMenuOpen(false)}>КУРСЫ</Link></li>
                 ) : isContactsPage ? (
-                  <li><Link to="/kurs">КУРСЫ</Link></li>
+                  <li><Link to="/kurs" onClick={() => setIsMenuOpen(false)}>КУРСЫ</Link></li>
                 ) : (
-                  <li><Link to="/contacts">О НАС</Link></li>
+                  <li><Link to="/contacts" onClick={() => setIsMenuOpen(false)}>О НАС</Link></li>
                 )}
 
-                {/* Третья ссылка - о нас (только на странице пробного урока) */}
                 {isFreeLessonPage && (
-                  <li><Link to="/contacts">О НАС</Link></li>
+                  <li><Link to="/contacts" onClick={() => setIsMenuOpen(false)}>О НАС</Link></li>
                 )}
 
-                {/* Всегда показываем личный кабинет */}
-                <li><Link to="/kabunet">ЛИЧНЫЙ КАБИНЕТ</Link></li>
+                <li><Link to="/kabunet" onClick={() => setIsMenuOpen(false)}>ЛИЧНЫЙ КАБИНЕТ</Link></li>
 
-                {/* Кнопка пробного урока (не показываем на странице freelesson) */}
                 {!isFreeLessonPage && (
-                  <li><Link to="/freelesson" className='zayavka'>ПРОБНЫЙ УРОК</Link></li>
+                  <li>
+                    <Link to="/freelesson" className='zayavka' onClick={() => setIsMenuOpen(false)}>
+                      ПРОБНЫЙ УРОК
+                    </Link>
+                  </li>
                 )}
               </ul>
             </div>
